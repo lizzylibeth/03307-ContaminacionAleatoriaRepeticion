cc09a172c568d3328803448114166aa253526a01
eli <eli@192.168.1.134>
2019-11-22 00:17:59 +0100

    :tada: Commit inicial

---
+++ src/org/japo/java/main/Main.java
@@ -0,0 +1,47 @@
+/* 
+ * Copyright 2019 Elísabet Palomino .
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.japo.java.main;
+
+import java.util.Locale;
+import java.util.Random;
+
+/**
+ *
+ * @author Elísabet Palomino
+ */
+public class Main {
+
+    public static final Random RND = new Random();
+
+    public static void main(String[] args) {
+
+        //Constantes
+        final double TEM_MIN = 0.00;
+        final double TEM_MAX = 100.00;
+        final int NUM_ESTACIONES = 4;
+
+        //Mensaje
+        for (int i = 0; i < NUM_ESTACIONES; i++) {
+
+            double contaminacion = RND.nextDouble() * (TEM_MAX - TEM_MIN) + TEM_MIN;
+
+            System.out.printf(Locale.ENGLISH, "Estación %d ...: %6.2f mcg N02 %n",
+                    i + 1, contaminacion);
+        }
+
+    }//main
+
+}//class
